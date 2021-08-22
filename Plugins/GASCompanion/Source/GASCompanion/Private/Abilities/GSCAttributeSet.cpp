fb5f9585e8564e2c99b79f7a042182552ff70224
dannykay1 <dannyjkay1@gmail.com>
2021-08-22 01:06:48 -0400

    blueprint updates

---
+++ Plugins/GASCompanion/Source/GASCompanion/Private/Abilities/GSCAttributeSet.cpp
@@ -0,0 +1,171 @@
+﻿// Copyright Danny Kay 2021.
+
+
+#include "Abilities/GSCAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+#include "Pawns/GSCCharacter.h"
+
+void UGSCAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	// If a Max value changes, adjust current to keep Current % of Current to Max
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		AdjustAttributeForMaxChange(Health, MaxHealth, NewValue, GetHealthAttribute());
+	}
+
+	if (Attribute == GetMaxArmorAttribute())
+	{
+		AdjustAttributeForMaxChange(Armor, MaxArmor, NewValue, GetArmorAttribute());
+	}
+}
+
+void UGSCAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	const FGameplayEffectContextHandle Context = Data.EffectSpec.GetContext();
+	UAbilitySystemComponent* Source = Context.GetOriginalInstigatorAbilitySystemComponent();
+	const FGameplayTagContainer& SourceTags = *Data.EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	FGameplayTagContainer SpecAssetTags;
+	Data.EffectSpec.GetAllAssetTags(SpecAssetTags);
+
+	// Get the Target actor, which should be our owner
+	AActor* TargetActor = nullptr;
+	AController* TargetController = nullptr;
+	AGSCCharacter* TargetCharacter = nullptr;
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		TargetActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		TargetCharacter = Cast<AGSCCharacter>(TargetActor);
+	}
+
+	// Get the Source actor
+	AActor* SourceActor = nullptr;
+	AController* SourceController = nullptr;
+	AGSCCharacter* SourceCharacter = nullptr;
+	if (Source && Source->AbilityActorInfo.IsValid() && Source->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		SourceActor = Source->AbilityActorInfo->AvatarActor.Get();
+		SourceController = Source->AbilityActorInfo->PlayerController.Get();
+		if (SourceController == nullptr && SourceActor != nullptr)
+		{
+			if (APawn* Pawn = Cast<APawn>(SourceActor))
+			{
+				SourceController = Pawn->GetController();
+			}
+		}
+
+		// Use the controller to find the source pawn
+		if (SourceController)
+		{
+			SourceCharacter = Cast<AGSCCharacter>(SourceController->GetPawn());
+		}
+		else
+		{
+			SourceCharacter = Cast<AGSCCharacter>(SourceActor);
+		}
+
+		// Set the causer actor based on context if it's set
+		if (Context.GetEffectCauser())
+		{
+			SourceActor = Context.GetEffectCauser();
+		}
+	}
+
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		// Store a local copy of the amount of damage done and clear the damage attribute
+		const float LocalDamageDone = GetDamage();
+		SetDamage(0.f);
+
+		if (LocalDamageDone > 0.0f)
+		{
+			// If character was alive before damage is added, handle damage
+			// This prevents damage being added to dead things and replaying death animations
+			const bool WasAlive = TargetCharacter ? TargetCharacter->IsAlive() : true;
+
+			// Apply the damage to shield first if it exists
+			const float OldArmor = GetArmor();
+			const float DamageAfterShield = LocalDamageDone - OldArmor;
+			if (OldArmor > 0)
+			{
+				const float NewShield = OldArmor - LocalDamageDone;
+				SetArmor(FMath::Clamp<float>(NewShield, 0.0f, GetMaxArmor()));
+			}
+
+			if (DamageAfterShield > 0)
+			{
+				// Apply the health change and then clamp it
+				const float NewHealth = GetHealth() - DamageAfterShield;
+				SetHealth(FMath::Clamp(NewHealth, 0.0f, GetMaxHealth()));
+			}
+
+			if (TargetCharacter && WasAlive)
+			{
+				// Show damage number for the Source player unless it was self damage
+				if (SourceActor != TargetActor)
+				{
+				}
+
+				if (!TargetCharacter->IsAlive())
+				{
+					// TargetCharacter was alive before this damage and now is not alive, give XP and Gold bounties to Source.
+					// Don't give bounty to self.
+					if (SourceController != TargetController)
+					{
+						// Create a dynamic instant Gameplay Effect to give the bounties
+						UGameplayEffect* GEBounty = NewObject<UGameplayEffect>(GetTransientPackage(), FName(TEXT("Bounty")));
+						GEBounty->DurationPolicy = EGameplayEffectDurationType::Instant;
+
+						const int32 Idx = GEBounty->Modifiers.Num();
+						GEBounty->Modifiers.SetNum(Idx + 2);
+
+						FGameplayModifierInfo& InfoXP = GEBounty->Modifiers[Idx];
+						InfoXP.ModifierMagnitude = FScalableFloat(GetXPBounty());
+						InfoXP.ModifierOp = EGameplayModOp::Additive;
+						InfoXP.Attribute = UGSCAttributeSet::GetXPAttribute();
+
+						FGameplayModifierInfo& InfoGold = GEBounty->Modifiers[Idx + 1];
+						InfoGold.ModifierMagnitude = FScalableFloat(GetGoldBounty());
+						InfoGold.ModifierOp = EGameplayModOp::Additive;
+						InfoGold.Attribute = UGSCAttributeSet::GetGoldAttribute();
+
+						Source->ApplyGameplayEffectToSelf(GEBounty, 1.0f, Source->MakeEffectContext());
+					}
+				}
+			}
+		}
+	} // Damage
+	else if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		// Handle other health changes.
+		// Health loss should go through Damage.
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+	} // Health
+	else if (Data.EvaluatedData.Attribute == GetArmorAttribute())
+	{
+		// Handle Armor changes.
+		SetArmor(FMath::Clamp(GetArmor(), 0.0f, GetMaxArmor()));
+	} // Armor
+}
+
+void UGSCAttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty) const
+{
+	UAbilitySystemComponent* AbilityComp = GetOwningAbilitySystemComponent();
+	const float CurrentMaxValue = MaxAttribute.GetCurrentValue();
+	if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue) && AbilityComp)
+	{
+		// Change current value to maintain the current Val / Max percent
+		const float CurrentValue = AffectedAttribute.GetCurrentValue();
+		const float NewDelta = (CurrentMaxValue > 0.f)
+								? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue
+								: NewMaxValue;
+
+		AbilityComp->ApplyModToAttributeUnsafe(AffectedAttributeProperty, EGameplayModOp::Additive, NewDelta);
+	}
+}
---
+++ Plugins/GASCompanion/Source/GASCompanion/Public/Abilities/GSCAttributeSet.h
@@ -0,0 +1,87 @@
+﻿// Copyright Danny Kay 2021.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "GSCAttributeSet.generated.h"
+
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/** Base class for all basic attributes */
+UCLASS()
+class GASCOMPANION_API UGSCAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	// Current Health, when 0 we expect owner to die unless prevented by an ability. Capped by MaxHealth.
+	// Positive changes can directly use this.
+	// Negative changes to Health should go through Damage meta attribute.
+	UPROPERTY(BlueprintReadOnly, Category = Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, Health)
+
+	// MaxHealth is its own attribute since GameplayEffects may modify it
+	UPROPERTY(BlueprintReadOnly, Category = Health)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, MaxHealth)
+
+	// Health regen rate will passively increase Health every second
+	UPROPERTY(BlueprintReadOnly, Category = Health)
+	FGameplayAttributeData HealthRegenRate;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, HealthRegenRate)
+
+	// Current armor acts like temporary health. When depleted, damage will drain regular health.
+	UPROPERTY(BlueprintReadOnly, Category = Armor)
+	FGameplayAttributeData Armor;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, Armor)
+
+	// Maximum Armor that we can have.
+	UPROPERTY(BlueprintReadOnly, Category = Armor)
+	FGameplayAttributeData MaxArmor;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, MaxArmor)
+
+	// Damage is a meta attribute used by the DamageExecution to calculate final damage, which then turns into -Health
+	UPROPERTY(BlueprintReadOnly, Category = Damage, meta = (HideFromLevelInfos))
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, Damage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Character Level")
+	FGameplayAttributeData CharacterLevel;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, CharacterLevel)
+
+	// Experience points gained from killing enemies. Used to level up (not implemented in this project).
+	UPROPERTY(BlueprintReadOnly, Category = XP)
+	FGameplayAttributeData XP;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, XP)
+
+	// Experience points awarded to the character's killers. Used to level up (not implemented in this project).
+	UPROPERTY(BlueprintReadOnly, Category = XP)
+	FGameplayAttributeData XPBounty;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, XPBounty)
+
+	// Gold gained from killing enemies. Used to purchase items.
+	UPROPERTY(BlueprintReadOnly, Category = Gold)
+	FGameplayAttributeData Gold;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, Gold)
+
+	// Gold awarded to the character's killer. Used to purchase items (not implemented in this project).
+	UPROPERTY(BlueprintReadOnly, Category = Gold)
+	FGameplayAttributeData GoldBounty;
+	ATTRIBUTE_ACCESSORS(UGSCAttributeSet, GoldBounty)
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+protected:
+	// Helper function to proportionally adjust the value of an attribute when it's associated max attribute changes.
+	// (i.e. When MaxHealth increases, Health increases by an amount that maintains the same percentage as before)
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty) const;
+};
